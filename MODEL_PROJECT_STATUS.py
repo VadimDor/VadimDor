bc3b1d458a2cf08a308f7f218c3415b118f36d64
Vadim Dor <V.Dorez@mail.ru>
2023-07-15 16:38:27 +0300

    dd

--- MODEL_PROJECT_STATUS.py
+++ MODEL_PROJECT_STATUS.py
@@ -19,10 +19,13 @@ def modelProjectStatus():
 
 def main():
     # print ("Model project status: This is a model project")
-    # print ("This is one of @vadimdor's Model projects. It is certified as a project ready for use as a first impression into vadimdor and his works")
+    # print ("This is one of @vadimdor's Model projects.
+    #  It is certified as a project ready for use as a first impression into 
+    # vadimdor and his works")
     # sleep(1)
     # noMore = input("Press [ENTER] key to exit")
-    # print("The program should now be closed. If you are not using Python in a terminal, and the program is still open, try manually closing it with the close button. If that doesn't work, try ending the process with a process/task manager.")
+    # print("The program should now be closed. If you are not using Python 
+    # in a terminal, and the program is still open, try manually closing it with the close button. If that doesn't work, try ending the process with a process/task manager.")
     return modelProjectStatus()
 
 
