5554356279d0003e8bfd68ddba75f774a03dbea2
Vadim Dor <V.Dorez@mail.ru>
2023-07-15 08:36:32 +0300

    vv

--- .github/BUILD_INFO_V1.py
+++ .github/BUILD_INFO_V1.py
@@ -22,7 +22,7 @@ def englishBuildInfo():
   print("Plain text version: Any")
   print("\nThese languages are in use for this project, and need to be installed to view the full project.")
   print("\nEnd of info")
-  break
+break
 def spanishBuildInfo():
   # Coming soon
   '''
@@ -43,112 +43,7 @@ def spanishBuildInfo():
   print("\nThese languages are in use for this project, and need to be installed to view the full project.")
   print("\nEnd of info")
   '''
-  break
-def esperantoBuildInfo():
-  # Coming soon
-  '''
-  print("Build information")
-  print("For: VadimDor/VadimDor")
-  languageList = ["Python", "HTML5", "SHell", "CSV", "Makefile", "TeX/BibTeX", "INI", "Desktop", "JSON", "YAML", "Markdown", "Plain Text"]
-  print("\nLanguages: " + str(languageList))
-  print("\nPython version: 3.6 and up")
-  print("BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)")
-  print("Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)")
-  print("Makefile (GNU Make) version: Unknown")
-  print("LaTeX/BiBTeX version: 2.0 or higher")
-  print("INI version: Windows 95 or newer")
-  print("KDE Desktop file version: 1.0 or newer")
-  print("JSON file version: 2001 or newer")
-  print("Markdown version: 1.0 or newer")
-  print("Plain text version: Any")
-  print("\nThese languages are in use for this project, and need to be installed to view the full project.")
-  print("\nEnd of info")
-  '''
-  break
-def frenchBuildInfo():
-  # Coming soon
-  '''
-  print("Build information")
-  print("For: VadimDor/VadimDor")
-  languageList = ["Python", "HTML5", "SHell", "CSV", "Makefile", "TeX/BibTeX", "INI", "Desktop", "JSON", "YAML", "Markdown", "Plain Text"]
-  print("\nLanguages: " + str(languageList))
-  print("\nPython version: 3.6 and up")
-  print("BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)")
-  print("Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)")
-  print("Makefile (GNU Make) version: Unknown")
-  print("LaTeX/BiBTeX version: 2.0 or higher")
-  print("INI version: Windows 95 or newer")
-  print("KDE Desktop file version: 1.0 or newer")
-  print("JSON file version: 2001 or newer")
-  print("Markdown version: 1.0 or newer")
-  print("Plain text version: Any")
-  print("\nThese languages are in use for this project, and need to be installed to view the full project.")
-  print("\nEnd of info")
-  '''
-  break
-def germanBuildInfo():
-  # Coming soon
-  '''
-  print("Build information")
-  print("For: VadimDor/VadimDor")
-  languageList = ["Python", "HTML5", "SHell", "CSV", "Makefile", "TeX/BibTeX", "INI", "Desktop", "JSON", "YAML", "Markdown", "Plain Text"]
-  print("\nLanguages: " + str(languageList))
-  print("\nPython version: 3.6 and up")
-  print("BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)")
-  print("Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)")
-  print("Makefile (GNU Make) version: Unknown")
-  print("LaTeX/BiBTeX version: 2.0 or higher")
-  print("INI version: Windows 95 or newer")
-  print("KDE Desktop file version: 1.0 or newer")
-  print("JSON file version: 2001 or newer")
-  print("Markdown version: 1.0 or newer")
-  print("Plain text version: Any")
-  print("\nThese languages are in use for this project, and need to be installed to view the full project.")
-  print("\nEnd of info")
-  '''
-  break
-def russianBuildInfo():
-  # Coming soon
-  '''
-  print("Build information")
-  print("For: VadimDor/VadimDor")
-  languageList = ["Python", "HTML5", "SHell", "CSV", "Makefile", "TeX/BibTeX", "INI", "Desktop", "JSON", "YAML", "Markdown", "Plain Text"]
-  print("\nLanguages: " + str(languageList))
-  print("\nPython version: 3.6 and up")
-  print("BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)")
-  print("Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)")
-  print("Makefile (GNU Make) version: Unknown")
-  print("LaTeX/BiBTeX version: 2.0 or higher")
-  print("INI version: Windows 95 or newer")
-  print("KDE Desktop file version: 1.0 or newer")
-  print("JSON file version: 2001 or newer")
-  print("Markdown version: 1.0 or newer")
-  print("Plain text version: Any")
-  print("\nThese languages are in use for this project, and need to be installed to view the full project.")
-  print("\nEnd of info")
-  '''
-  break
-def arabicBuildInfo():
-  # Coming soon
-  '''
-  print("Build information")
-  print("For: VadimDor/VadimDor")
-  languageList = ["Python", "HTML5", "SHell", "CSV", "Makefile", "TeX/BibTeX", "INI", "Desktop", "JSON", "YAML", "Markdown", "Plain Text"]
-  print("\nLanguages: " + str(languageList))
-  print("\nPython version: 3.6 and up")
-  print("BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)")
-  print("Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)")
-  print("Makefile (GNU Make) version: Unknown")
-  print("LaTeX/BiBTeX version: 2.0 or higher")
-  print("INI version: Windows 95 or newer")
-  print("KDE Desktop file version: 1.0 or newer")
-  print("JSON file version: 2001 or newer")
-  print("Markdown version: 1.0 or newer")
-  print("Plain text version: Any")
-  print("\nThese languages are in use for this project, and need to be installed to view the full project.")
-  print("\nEnd of info")
-  '''
-  break
+break
 # Language selector
 langInput1 = str(input("Lang🌐 >> "))
 langInput1 == langInput1.upper()
@@ -157,21 +52,6 @@ if (langInput1 == "EN" or "ENGLISH"):
   break
 elif (langInput1 == "ES" or "SPANISH"):
   return spanishBuildInfo()
-  break
-elif (langInput1 == "EO" or "ESPERANTO"):
-  return esperantoBuildInfo()
-  break
-elif (langInput1 == "FR" or "FRENCH"):
-  return frenchBuildInfo()
-  break
-elif (langInput1 == "DE" or "GERMAN"):
-  return germanBuildInfo()
-  break
-elif (langInput1 == "RU" or "RUSSIAN"):
-  return russianBuildInfo()
-  break
-elif (langInput1 == "AR" or "ARABIC"):
-  return arabicBuildInfo()
   break
 else:
   print("That language is not available")
@@ -187,12 +67,4 @@ List is out of order
 Not all intended languages are supported yet
 '''
 # This project uses ISO 639-1 language codes
-"""
-File info
-
-File type: Python source file (*.py)
-File version: 1 (2023, Friday, January 7th at 5:57 pm)
-Line count (including blank lines and compiler line): 1,237
-File language: English (US)
-"""
 # End of script
