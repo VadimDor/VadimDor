aa1db4c4e376b1221ae570e56138c3df0dbc19d8
Vadim Dor <V.Dorez@mail.ru>
2023-07-16 01:43:41 +0300

    vvv

--- MODEL_PROJECT_STATUS.py
+++ MODEL_PROJECT_STATUS.py
@@ -7,7 +7,7 @@ Information about this projects use as a model project
 
 from asyncio import sleep
 
-
+# modelProjectStatus
 def modelProjectStatus():
     print('Model project status: This is a model project')
     print(
@@ -16,18 +16,17 @@ def modelProjectStatus():
         into vadimdor and his works"
     )
     sleep(1)
-    return
 
-
+# main
 def main():
     # print ("Model project status: This is a model project")
     # print ("This is one of @vadimdor's Model projects.
-    #  It is certified as a project ready for use as a first impression into 
+    #  It is certified as a project ready for use as a first impression into
     # vadimdor and his works")
     # sleep(1)
     # noMore = input("Press [ENTER] key to exit")
-    # print("The program should now be closed. If you are not using Python 
-    # in a terminal, and the program is still open, try manually closing it with the close button. 
+    # print("The program should now be closed. If you are not using Python
+    # in a terminal, and the program is still open, try manually closing it with the close button.
     # If that doesn't work, try ending the process with a process/task manager.")
     return modelProjectStatus()
 
