8c5b2992315d66d2962403414dca94b7c93b1601
Lint Action <github-actions[bot]@users.noreply.github.com>
2023-07-15 08:14:00 +0000

    Fix code style issues with Black

--- BUILD_INFO.py
+++ BUILD_INFO.py
@@ -1,69 +1,93 @@
 #!/usr/bin/env python
 # Start of script
-'''
+"""
 BUILD_INFO.py
 Build information file for this project
-'''
+"""
+
+
 # This file has not yet been tested
 def englishBuildInfo():
-  print("Build information")
-  print("For: VadimDor/VadimDor")
-  languageList = ["Python", "HTML5", "SHell", "CSV", "Makefile", "TeX/BibTeX", "INI", "Desktop", "JSON", "YAML", "Markdown", "Plain Text"]
-  print("\nLanguages: " + str(languageList))
-  print("\nPython version: 3.6 and up")
-  print("BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)")
-  print("Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)")
-  print("Makefile (GNU Make) version: Unknown")
-  print("LaTeX/BiBTeX version: 2.0 or higher")
-  print("INI version: Windows 95 or newer")
-  print("KDE Desktop file version: 1.0 or newer")
-  print("JSON file version: 2001 or newer")
-  print("Markdown version: 1.0 or newer")
-  print("Plain text version: Any")
-  print("\nThese languages are in use for this project, and need to be installed to view the full project.")
-  print("\nEnd of info")
-  return
+    print("Build information")
+    print("For: VadimDor/VadimDor")
+    languageList = [
+        "Python",
+        "HTML5",
+        "SHell",
+        "CSV",
+        "Makefile",
+        "TeX/BibTeX",
+        "INI",
+        "Desktop",
+        "JSON",
+        "YAML",
+        "Markdown",
+        "Plain Text",
+    ]
+    print("\nLanguages: " + str(languageList))
+    print("\nPython version: 3.6 and up")
+    print(
+        "BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)"
+    )
+    print(
+        "Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)"
+    )
+    print("Makefile (GNU Make) version: Unknown")
+    print("LaTeX/BiBTeX version: 2.0 or higher")
+    print("INI version: Windows 95 or newer")
+    print("KDE Desktop file version: 1.0 or newer")
+    print("JSON file version: 2001 or newer")
+    print("Markdown version: 1.0 or newer")
+    print("Plain text version: Any")
+    print(
+        "\nThese languages are in use for this project, and need to be installed to view the full project."
+    )
+    print("\nEnd of info")
+    return
+
 
 def spanishBuildInfo():
-  # Coming soon
-  '''
-  print("Build information")
-  print("For: VadimDor/VadimDor")
-  languageList = ["Python", "HTML5", "SHell", "CSV", "Makefile", "TeX/BibTeX", "INI", "Desktop", "JSON", "YAML", "Markdown", "Plain Text"]
-  print("\nLanguages: " + str(languageList))
-  print("\nPython version: 3.6 and up")
-  print("BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)")
-  print("Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)")
-  print("Makefile (GNU Make) version: Unknown")
-  print("LaTeX/BiBTeX version: 2.0 or higher")
-  print("INI version: Windows 95 or newer")
-  print("KDE Desktop file version: 1.0 or newer")
-  print("JSON file version: 2001 or newer")
-  print("Markdown version: 1.0 or newer")
-  print("Plain text version: Any")
-  print("\nThese languages are in use for this project, and need to be installed to view the full project.")
-  print("\nEnd of info")
-  '''
-  return
+    # Coming soon
+    """
+    print("Build information")
+    print("For: VadimDor/VadimDor")
+    languageList = ["Python", "HTML5", "SHell", "CSV", "Makefile", "TeX/BibTeX", "INI", "Desktop", "JSON", "YAML", "Markdown", "Plain Text"]
+    print("\nLanguages: " + str(languageList))
+    print("\nPython version: 3.6 and up")
+    print("BASH SHell verson: 5.0 and up (older versions may still work perfectly fine)")
+    print("Comma Seperated Value version: Any (recommended programs: LibreOffice Calc, Kate, Notepad++, Gedit, any other text editor)")
+    print("Makefile (GNU Make) version: Unknown")
+    print("LaTeX/BiBTeX version: 2.0 or higher")
+    print("INI version: Windows 95 or newer")
+    print("KDE Desktop file version: 1.0 or newer")
+    print("JSON file version: 2001 or newer")
+    print("Markdown version: 1.0 or newer")
+    print("Plain text version: Any")
+    print("\nThese languages are in use for this project, and need to be installed to view the full project.")
+    print("\nEnd of info")
+    """
+    return
+
 
 def main():
-# Language selector
- langInput1 = str(input("Lang🌐 >> "))
- langInput1 == langInput1.upper()
- if (langInput1 == "EN" or "ENGLISH"):
-   return englishBuildInfo()
-   #break
- elif (langInput1 == "ES" or "SPANISH"):
-   return spanishBuildInfo()
-   #break
- else:
-   print("That language is not available")
-   noMore = input("❌")
-   print("Goodbye")
+    # Language selector
+    langInput1 = str(input("Lang🌐 >> "))
+    langInput1 == langInput1.upper()
+    if langInput1 == "EN" or "ENGLISH":
+        return englishBuildInfo()
+        # break
+    elif langInput1 == "ES" or "SPANISH":
+        return spanishBuildInfo()
+        # break
+    else:
+        print("That language is not available")
+        noMore = input("❌")
+        print("Goodbye")
 
-if __name__ == '__main__':
-    main()   
-'''
+
+if __name__ == "__main__":
+    main()
+"""
 Problems
 Language name inputs are not translated to the correct language
 Exit function is not translated to a non-English language
@@ -71,6 +95,6 @@ The program may not yet be functional yet
 Translations are needed for all languages except for English
 List is out of order
 Not all intended languages are supported yet
-'''
+"""
 # This project uses ISO 639-1 language codes
 # End of script
--- MODEL_PROJECT_STATUS.py
+++ MODEL_PROJECT_STATUS.py
@@ -9,17 +9,21 @@ from asyncio import sleep
 
 
 def modelProjectStatus():
-  print ("Model project status: This is a model project")
-  print ("This is one of @vadimdor's Model projects. It is certified as a project ready for use as a first impression into vadimdor and his works")
-  sleep(1)
-  return
+    print("Model project status: This is a model project")
+    print(
+        "This is one of @vadimdor's Model projects. It is certified as a project ready for use as a first impression into vadimdor and his works"
+    )
+    sleep(1)
+    return
+
 
 def main():
-# print ("Model project status: This is a model project")
-# print ("This is one of @vadimdor's Model projects. It is certified as a project ready for use as a first impression into vadimdor and his works")
-# sleep(1)
- #noMore = input("Press [ENTER] key to exit")
- #print("The program should now be closed. If you are not using Python in a terminal, and the program is still open, try manually closing it with the close button. If that doesn't work, try ending the process with a process/task manager.")
- return modelProjectStatus()
+    # print ("Model project status: This is a model project")
+    # print ("This is one of @vadimdor's Model projects. It is certified as a project ready for use as a first impression into vadimdor and his works")
+    # sleep(1)
+    # noMore = input("Press [ENTER] key to exit")
+    # print("The program should now be closed. If you are not using Python in a terminal, and the program is still open, try manually closing it with the close button. If that doesn't work, try ending the process with a process/task manager.")
+    return modelProjectStatus()
+
 
 # End of script
