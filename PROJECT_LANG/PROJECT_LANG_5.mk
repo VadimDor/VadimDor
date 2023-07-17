48e1042a58546524a7c9ff745dd29a72a1d1c2d3
Vadim Dor <V.Dorez@mail.ru>
2023-07-17 20:12:43 +0300

    PROJECT_LANG deleted

--- PROJECT_LANG/PROJECT_LANG_5.mk
+++
@@ -1,9 +0,0 @@
-# Start of script
-# Project language file 5
-# For: VadimDor/VadimDor
-# I decided to make GNU Make the 5th project language file for this project (VadimDor/VadimDor) as makefile is one of the software building languages used for this project, and I am representing all project languages today (2023, Sunday, January 2nd)
-# File info
-# File type: Makefile (MAKEFILE, *.mk *.mak *.make)
-# File version: 1 (Sunday, 2023 January 2nd at 4:25 pm)
-# Line count (including blank lines and compiler line): 10
-# End of script
